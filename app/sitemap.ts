5fce6cd48fd12e3ea60cf11c9235bdcf948fb13f
Delphine Ravet <delphine@horizonpmesolutions.fr>
2026-04-16 13:52:06 +0200

    sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://www.horizonpmesolutions.fr'
+  const baseUrl = 'https://horizonpmesolutions.fr'
   const lastModified = new Date()
 
   return [
